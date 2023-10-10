f860b1e0a678341d5f25cfd309cd6fded62a7ac9
Hytia49 <45233054+Hytia49@users.noreply.github.com>
2023-10-10 10:37:33 +0200

    Delete Reinforcement_Learning_TICTACTOE_Project directory

--- Reinforcement_Learning_TICTACTOE_Project/tictactoe-project/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-from pathlib import Path
-
-DIR_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-PLAYERS_DATA_TEST_PATH = os.path.join(DIR_PATH,"tictactoe-main", "players")
